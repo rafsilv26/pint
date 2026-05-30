645644b93b23ddfdbf69d793088cb2064b77994b
Miguel <miguelrbarbosa05@gmail.com>
2026-05-30 15:45:48 +0100

    corrigido rota de service line

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,7 +6,6 @@ const { protect, authorize } = require('../middlewares/authMiddleware');
 
 router.use('/auth', require('./authRoutes'));
 router.use('/users', protect, authorize('Admin'), require('./userRoutes'));
-router.use('/candidaturas', protect, authorize('Consultor', 'TalentManager'), require('./candidaturaRoutes'));
 router.use('/relatorios', protect, authorize('Admin', 'TalentManager', 'ServiceLineLeader'), require('./relatorioRoutes'));
 
 module.exports = router;
