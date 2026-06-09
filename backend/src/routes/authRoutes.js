8465e1bf3f95c3fe20e94e9695c904e42737ca40
Simão Costa <simadinisvc@gmail.com>
2026-06-09 19:18:24 +0100

    error fixed

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
 // O registo de utilizadores é feito por um administrador autenticado.
-router.post('/register', protect, authController.register);
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/me', protect, authController.me);
 router.put('/change-password', protect, authController.changePassword);
