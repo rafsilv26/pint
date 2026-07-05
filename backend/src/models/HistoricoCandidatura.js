c24f5efa95de3b113fbfa1a4dc4568b4c38cc9ff
Miguel <miguelrbarbosa05@gmail.com>
2026-07-05 11:38:02 +0100

    Tentativa de resolução candidatura historico

--- backend/src/models/HistoricoCandidatura.js
+++ backend/src/models/HistoricoCandidatura.js
@@ -3,38 +3,44 @@ const sequelize = require('../config/database');
 
 const HistoricoCandidatura = sequelize.define('HistoricoCandidatura', {
   id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          field: 'logwf_id' 
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'logwf_id' // Traduz 'id' no JS para 'logwf_id' na BD
   },
   candidaturaId: {
     type: DataTypes.INTEGER,
-    allowNull: true
+    allowNull: true,
+    field: 'candidaturaid'
   },
   userId: {
     type: DataTypes.INTEGER,
-    allowNull: true
+    allowNull: true,
+    field: 'userid'
   },
   estadoAnterior: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    field: 'antigo_estadoid' // Traduz 'estadoAnterior' no JS para 'antigo_estadoid' na BD
   },
   estadoNovo: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    field: 'novo_estadoid' // Traduz 'estadoNovo' no JS para 'novo_estadoid' na BD
   },
   motivo: {
     type: DataTypes.STRING(500),
-    allowNull: true
+    allowNull: true,
+    field: 'motivo'
   },
   createdAt: {
     type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+    defaultValue: DataTypes.NOW,
+    field: 'created_at'
   }
 }, {
   tableName: 'LOGSWORKFLOW_CANDIDATURABADGE',
-  timestamps: false
+  timestamps: false // Desligamos os timestamps automáticos porque já mapeámos o createdAt acima
 });
 
 module.exports = HistoricoCandidatura;
