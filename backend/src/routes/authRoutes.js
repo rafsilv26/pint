cfeac0792733d2b19c088d54efa5e04e00dc846b
rafsilv26 <102412628+rafsilv26@users.noreply.github.com>
2026-07-23 00:54:32 +0100

    Add protection and authorization to register route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
-router.post('/register', authController.register); // protect, authorize('Admin'),
+router.post('/register', protect, authorize('Admin'), authController.register); 
 router.post('/signup', authController.signup);
 router.post('/resend-confirmation', authController.resendConfirmation);
 router.get('/areas', authController.listarAreasPublicas);
